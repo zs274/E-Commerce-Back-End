59aa2c4de8acb7481f9a2beb895a6b6d86a1faf5
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-19 02:28:44 +0000

    formatted code

--- models/Product.js
+++ models/Product.js
@@ -4,15 +4,15 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
-  { 
+  {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false, 
-      primaryKey: true, 
+      allowNull: false,
+      primaryKey: true,
       autoIncrement: true
     },
     product_name: {
@@ -42,7 +42,7 @@ Product.init(
         unique: false
       },
     },
-   
+
   },
   {
     sequelize,
