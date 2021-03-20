8c61acc2f45a6309eea0bfd0ba0362a4737f94f0
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-20 01:34:29 +0000

    added create, update and delete functions

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,16 +31,50 @@ try {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+outer.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).then(tagData => {
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id'});
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id 
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
