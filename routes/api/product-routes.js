3991c15460f6c5c692f4b73be7181adcd4efc6cc
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-20 01:18:47 +0000

    imported sequelize connection, updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, through: Tag, as: 'selected_tag' }],
+      include: [{ model: Tag, Category, through: ProductTag, as: 'product_tags' }],
     });
     if (!productData) {
       res.status(404).json({ message: 'No tag found with this id'});
